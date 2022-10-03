2b82b3bae124e22b496da19a04a6c1db053e9079
alezana <azanardi@ethz.ch>
2022-10-03 19:55:45 +0200

    timeout (to be tested)

---
+++ src/pdm4ar/exercises_def/structures_time.py
@@ -0,0 +1,45 @@
+import multiprocessing
+from functools import wraps
+
+
+class TimeExceededException(Exception):
+    pass
+
+
+def function_runner(*args, **kwargs):
+    """Used as a wrapper function to handle
+    returning results on the multiprocessing side"""
+
+    send_end = kwargs.pop("__send_end")
+    function = kwargs.pop("__function")
+    try:
+        result = function(*args, **kwargs)
+    except Exception as e:
+        send_end.send(e)
+        return
+    send_end.send(result)
+
+
+@parametrized
+def run_with_timer(func, max_execution_time):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        recv_end, send_end = multiprocessing.Pipe(False)
+        kwargs["__send_end"] = send_end
+        kwargs["__function"] = func
+
+        p = multiprocessing.Process(target=function_runner, args=args, kwargs=kwargs)
+        p.start()
+        p.join(max_execution_time)
+        if p.is_alive():
+            p.terminate()
+            p.join()
+            raise TimeExceededException("Exceeded Execution Time")
+        result = recv_end.recv()
+
+        if isinstance(result, Exception):
+            raise result
+
+        return result
+
+    return wrapper
