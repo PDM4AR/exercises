d0175099d7e5d04f1bd551ab741e9565e7e15466
Alessandro Zanardi <azanardi@ethz.ch>
2022-10-05 21:15:24 +0200

    ids (#11)

--- src/pdm4ar/available_exercises.py
+++ src/pdm4ar/available_exercises.py
@@ -7,10 +7,10 @@ from pdm4ar.exercises_def.structures import Exercise
 
 available_exercises: Mapping[str, Callable[[], Exercise]] = frozendict(
     {
-        "exercise1": get_exercise1,
-        "exercise2": get_exercise2,
-        "exercise3": get_exercise3,
-        "exercise4": get_exercise4,
+        "01": get_exercise1,
+        "02": get_exercise2,
+        "03": get_exercise3,
+        "04": get_exercise4,
         "collision_check": get_exercise_collision_check,
         "opt-milp": get_exercise_optimization_milp,
     }
