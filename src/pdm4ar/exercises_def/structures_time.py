c1ea940008869afb6279d75a1788869c074267cc
Alessandro Zanardi <azanardi@ethz.ch>
2022-10-24 00:08:36 +0200

    Memory limit (#31)
    Co-authored-by: Jason, HU Shengjie <jasonhu515@gmail.com>

--- src/pdm4ar/exercises_def/structures_time.py
+++ src/pdm4ar/exercises_def/structures_time.py
@@ -3,7 +3,7 @@ import multiprocessing
 import traceback
 from functools import wraps
 from pdm4ar.exercises_def import logger
-
+from pdm4ar.exercises_def.structures_memory import set_memory_limit, MemoryLimitExceededException
 
 class TestCaseTimeoutException(Exception):
     pass
@@ -15,8 +15,16 @@ def function_runner(*args, **kwargs):
 
     send_end = kwargs.pop("__send_end")
     function = kwargs.pop("__function")
+    ########
+    set_memory_limit()  # Limited to 8GB on the evaluation server
+    ########
     try:
         result = function(*args, **kwargs)
+    except MemoryError:
+        ret = MemoryLimitExceededException("Memory limit exceeded.")
+        send_end.send(ret)
+        logger.warn(f"{ret}")
+        return
     except Exception as e:
         send_end.send(e)
         logger.warn(f"Failed because of:\n {e.args} \n{''.join(traceback.format_tb(e.__traceback__))}")
@@ -24,7 +32,7 @@ def function_runner(*args, **kwargs):
     send_end.send(result)
 
 
-def run_with_timer(func, max_execution_time) -> Union[Any,Exception]:
+def run_with_timer(func, max_execution_time) -> Union[Any, Exception]:
     @wraps(func)
     def wrapper(*args, **kwargs):
         recv_end, send_end = multiprocessing.Pipe(False)
