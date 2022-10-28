f3bec18277591b7d787418ef85a21f9e276ce506
alezana <azanardi@ethz.ch>
2022-10-28 22:29:41 +0200

    minors

--- src/pdm4ar/exercises/ex05/algo.py
+++ src/pdm4ar/exercises/ex05/algo.py
@@ -28,7 +28,8 @@ class Dubins(PathPlanner):
         :return: a List[SE2Transform] of configurations in the optimal path the car needs to follow
         """
         self.path = calculate_dubins_path(start_config=start, end_config=end, radius=self.params.min_radius)
-        se2_list = extract_path_points(self.path) 
+        # fixme why self?!
+        se2_list = extract_path_points(self.path)
         return se2_list
 
 
@@ -45,6 +46,7 @@ class ReedsShepp(PathPlanner):
         :return: a List[SE2Transform] of configurations in the optimal path the car needs to follow 
         """
         self.path = calculate_reeds_shepp_path(start_config=start, end_config=end, radius=self.params.min_radius)
+        # same as above
         se2_list = extract_path_points(self.path)
         return se2_list
 
@@ -56,22 +58,23 @@ def calculate_car_turning_radius(wheel_base: float, max_steering_angle: float) -
 
 def calculate_turning_circles(current_config: SE2Transform, radius: float) -> TurningCircle:
     # TODO implement here your solution
-    dummy_circle = Curve.create_circle(center=SE2Transform.identity(), config_on_circle=SE2Transform.identity, radius=0.1, curve_type=DubinsSegmentType.LEFT) # TODO remove
+    dummy_circle = Curve.create_circle(center=SE2Transform.identity(), config_on_circle=SE2Transform.identity,
+                                       radius=0.1, curve_type=DubinsSegmentType.LEFT)  # TODO remove
     return TurningCircle(left=dummy_circle, right=dummy_circle)
 
 
 def calculate_tangent_btw_circles(circle_start: Curve, circle_end: Curve) -> List[Line]:
     # TODO implement here your solution
-    return [] # i.e., [Line(),...]
+    return []  # i.e., [Line(),...]
 
 
 def calculate_dubins_path(start_config: SE2Transform, end_config: SE2Transform, radius: float) -> Path:
     # TODO implement here your solution
     # Please keep segments with zero length in the return list & return a valid dubins path!
-    return [] # e.g., [Curve(), Line(),..]
+    return []  # e.g., [Curve(), Line(),..]
 
 
 def calculate_reeds_shepp_path(start_config: SE2Transform, end_config: SE2Transform, radius: float) -> Path:
     # TODO implement here your solution
     # Please keep segments with zero length in the return list & return a valid dubins/reeds path!
-    return [] # e.g., [Curve(..,gear=Gear.REVERSE), Curve(),..]
+    return []  # e.g., [Curve(..,gear=Gear.REVERSE), Curve(),..]
