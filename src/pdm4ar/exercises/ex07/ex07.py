4dd37da2af9fb7630ca4ca7d7eafb85d62520d6b
Nicola Loi <loinicola98@gmail.com>
2022-11-06 12:28:25 +0000

    Multiple final little changes

--- src/pdm4ar/exercises/ex07/ex07.py
+++ src/pdm4ar/exercises/ex07/ex07.py
@@ -1,31 +1,40 @@
 import numpy as np
-from pdm4ar.exercises_def.ex07.structures import ProblemVoyage, OptimizationCost, Island, Constraints, MilpFeasibility, ProblemSolution
+
+from pdm4ar.exercises_def.ex07.structures import (
+    ProblemVoyage,
+    OptimizationCost,
+    Island,
+    Constraints,
+    MilpFeasibility,
+    SolutionVoyage,
+)
 
 
-def solve_optimization(problem: ProblemVoyage) -> ProblemSolution:
+def solve_optimization(problem: ProblemVoyage) -> SolutionVoyage:
     """
-    Solve the optimization problem enforcing the active constraints
+    Solve the optimization problem enforcing the active constraints.
 
     Parameters
     ---
     problem : ProblemVoyage
-        Contains the problem data: cost to optimize, starting crew, tuple of islands, 
-        and information about active constraints.
+        Contains the problem data: cost to optimize, starting crew, tuple of islands,
+        and information about active constraint (the constraints not set to `None` +
+        the `voyage_order` constraint)
 
     Returns
     ---
-    out : ProblemSolution
+    out : SolutionVoyage
         Contains the feasibility status of the problem, and the optimal voyage plan
-        as a list of ints if problem is feasible, else None
+        as a list of ints if problem is feasible, else `None`.
     """
 
     # toy examples with random voyage plans
     np.random.seed(None)
     if np.random.random() > 0.3:
         feasibility = MilpFeasibility.feasible
-        voyage_plan = list(np.random.randint(0,len(problem.islands), size=(min(7,len(problem.islands)),)))
+        voyage_plan = list(np.random.randint(0, len(problem.islands), size=(min(7, len(problem.islands)),)))
     else:
         feasibility = MilpFeasibility.unfeasible
         voyage_plan = None
 
-    return ProblemSolution(feasibility, voyage_plan)
+    return SolutionVoyage(feasibility, voyage_plan)
