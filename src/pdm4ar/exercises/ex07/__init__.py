c4cb311b86134ba03f1d56c5deffbd65b43e4dc7
Nicola <nicloi@student.ethz.ch>
2022-11-30 16:18:31 +0000

    Add cvxopt library required by cvxpy

--- src/pdm4ar/exercises/ex07/__init__.py
+++ src/pdm4ar/exercises/ex07/__init__.py
@@ -1,8 +1,10 @@
 # Please do not modify. Needed by the server to inform you if you should run "make update".
-__ex_version__ = "7.1.0"
+__ex_version__ = "7.2.0"
 
 
 # LOG
+
+# 7.2.0: add cvxopt library required by cvxpy in some optimization problems
 
 # 7.1.0: revert timeout of p.join() to solve wrong Exception info
 
