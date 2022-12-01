f8f07cfc1a973b6e7030c1998a5c6c03ab8decf9
Nicola Loi <79461707+loinicola@users.noreply.github.com>
2022-12-01 10:28:46 +0100

    Forcing cvxpy == 1.2.2 (#54)
    Co-authored-by: Nicola <nicloi@student.ethz.ch>

--- src/pdm4ar/exercises/ex07/__init__.py
+++ src/pdm4ar/exercises/ex07/__init__.py
@@ -1,8 +1,10 @@
 # Please do not modify. Needed by the server to inform you if you should run "make update".
-__ex_version__ = "7.2.0"
+__ex_version__ = "7.3.0"
 
 
 # LOG
+
+# 7.3.0: force cvxpy version 1.2.2
 
 # 7.2.0: add cvxopt and cylp libraries required to use cvxpy correctly - doc updated
 
