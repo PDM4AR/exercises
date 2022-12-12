577816eb4463e88cbecdfd9d5372889a1be480bd
alezana <azanardi@ethz.ch>
2022-12-11 20:41:47 -0500

    dev

--- src/pdm4ar/exercises_def/ex08/sim_context.py
+++ src/pdm4ar/exercises_def/ex08/sim_context.py
@@ -1,45 +1,37 @@
 from collections import defaultdict
 from copy import deepcopy
 from decimal import Decimal as D
-from typing import Optional, Dict
+from typing import Dict
 
-from dg_commons import PlayerName, DgSampledSequence
-from dg_commons.maps.shapes_generator import create_random_starshaped_polygon
+from dg_commons import DgSampledSequence
 from dg_commons.perception.sensor import VisRangeSensor
-from dg_commons.planning import PlanningGoal
-from dg_commons.sim import SimParameters, SimModel
+from dg_commons.sim import SimParameters
 from dg_commons.sim.agents import NPAgent
 from dg_commons.sim.models.obstacles import ObstacleGeometry, DynObstacleParameters
 from dg_commons.sim.models.obstacles_dyn import DynObstacleModel, DynObstacleState, DynObstacleCommands
-from dg_commons.sim.models.vehicle import VehicleModel, VehicleState
-from dg_commons.sim.scenarios.structures import DgScenario
+from dg_commons.sim.models.vehicle import VehicleModel
 from dg_commons.sim.sim_perception import ObsFilter, IdObsFilter, FovObsFilter
 from dg_commons.sim.simulator import SimContext
-from numpy import deg2rad
-from shapely.geometry import Polygon
 
 from pdm4ar.exercises.ex08.agent import Pdm4arAgent
-from pdm4ar.exercises_def.ex08.scenario import get_dgscenario
+from pdm4ar.exercises_def.ex08.scenario import *
 
 __all__ = ["get_sim_context_static", "get_sim_context_dynamic"]
 
-PDM4AR = PlayerName("PDM4AR")
 
-
-def _get_sim_context_static(scenario: DgScenario, goal: PlanningGoal, x0: VehicleState) -> SimContext:
+def _get_sim_context_static(scenario: DgScenario, goals: Goals, x0: VehicleState) -> SimContext:
     model = VehicleModel.default_car(x0)
-    models: Dict[PlayerName, VehicleModel] = {PDM4AR: model}
-    missions = {PDM4AR: goal}
-    players = {PDM4AR: Pdm4arAgent(
-            goal=goal,
-            static_obstacles=deepcopy(list(scenario.static_obstacles.values())),
+    models: Dict[PlayerName, VehicleModel] = {PDM4AR_1: model}
+    # missions = goals
+    players = {PDM4AR_1: Pdm4arAgent(
             sg=deepcopy(model.model_geometry),
-            sp=deepcopy(model.model_params))
+            sp=deepcopy(model.model_params)
+    )
     }
     # sensing
     lidar2d = VisRangeSensor(range=40)
     sensors: Dict[PlayerName, ObsFilter] = defaultdict(lambda: IdObsFilter())
-    sensors[PDM4AR] = FovObsFilter(lidar2d)
+    sensors[PDM4AR_1] = FovObsFilter(lidar2d)
     # sim parameters
     sim_params = SimParameters(dt=D("0.01"),
                                dt_commands=D("0.1"),
@@ -50,14 +42,14 @@ def _get_sim_context_static(scenario: DgScenario, goal: PlanningGoal, x0: Vehicl
             dg_scenario=scenario,
             models=models,
             players=players,
-            missions=missions,
+            missions=goals,
             sensors=sensors,
             param=sim_params)
 
 
 def get_sim_context_static(seed: Optional[int] = None) -> SimContext:
-    dgscenario, goal, x0 = get_dgscenario(seed)
-    simcontext = _get_sim_context_static(dgscenario, goal, x0)
+    dgscenario, goals, x0 = get_dgscenario(seed)
+    simcontext = _get_sim_context_static(dgscenario, goals, x0)
     simcontext.description = "static-environment"
     return simcontext
 
@@ -66,9 +58,10 @@ def get_sim_context_dynamic(seed: Optional[int] = None) -> SimContext:
     dgscenario, goal, x0 = get_dgscenario(seed)
     simcontext = _get_sim_context_static(dgscenario, goal, x0)
     simcontext.description = "dynamic-environment"
-    # add a couple of dynamic obstacles to the environment
+
+    # add a couple of non-reactive agents to the environment
     DObs1 = PlayerName("DObs1")
-    poly = Polygon(create_random_starshaped_polygon(0, 0, 3, 0.2, 0.2, 6))
+    poly = Polygon(create_random_starshaped_polygon(0, 0, 2, 0.1, 0.1, 6))
     x0_dobs1: DynObstacleState = DynObstacleState(x=60, y=80, psi=deg2rad(-70), vx=6, vy=0, dpsi=0)
     og_dobs1: ObstacleGeometry = ObstacleGeometry(m=1000, Iz=1000, e=0.2)
     op_dops1: DynObstacleParameters = DynObstacleParameters(vx_limits=(-10, 10), acc_limits=(-1, 1))
@@ -82,4 +75,6 @@ def get_sim_context_dynamic(seed: Optional[int] = None) -> SimContext:
     simcontext.models.update(models)
     simcontext.players.update(players)
 
+    # add embodied clones of the nominal agent
+    # todo
     return simcontext
