c7d1a43e9a5b77e4152758bc91e095d640829b7c
alezana <azanardi@ethz.ch>
2023-11-25 18:02:27 +0000

    minors

--- src/pdm4ar/exercises_def/ex09/config_reader.py
+++ src/pdm4ar/exercises_def/ex09/config_reader.py
@@ -5,7 +5,8 @@ from numpy import arctan2
 
 from typing import Any
 import yaml
-from shapely import LineString, Point, BaseGeometry
+from shapely import LineString, Point
+from shapely.geometry.base import BaseGeometry
 from dg_commons.sim import SimParameters
 from dg_commons.sim.agents import NPAgent
 from dg_commons.sim.models.rocket import RocketState, RocketCommands, RocketModel
@@ -22,7 +23,6 @@ from dg_commons.sim.models.obstacles_dyn import (
 )
 
 from dg_commons.sim.scenarios.structures import DgScenario
-
 from dg_commons import PlayerName, DgSampledSequence
 from pdm4ar.exercises.ex09.agent import RocketAgent
 from pdm4ar.exercises_def.ex09.goal import RocketTarget
@@ -36,20 +36,22 @@ def _load_config(file_path: str) -> dict[str, Any]:
 
 def _parse_planets(
     c: dict,
-) -> tuple[list[BaseGeometry], list[DynObstacleModel], list[NPAgent]]:
+) -> tuple[
+    list[BaseGeometry], dict[PlayerName, DynObstacleModel], dict[PlayerName, NPAgent]
+]:
     planets = []
-    satellites = []
-    satellites_players = []
+    satellites = {}
+    satellites_players = {}
 
     for p in c["planets"]:
         planet = Point(p["center"]).buffer(p["radius"])
         planets.append(planet)
-        for s in p["satellites"]:
+        for sname, s in p["satellites"].items():
             s, ps = _parse_satellite(
                 planet, tau=s["tau"], orbit_r=s["orbit_r"], omega=s["omega"]
             )
-            satellites.append(s)
-            satellites_players.append(ps)
+            satellites[sname] = s
+            satellites_players[sname] = ps
 
     return planets, satellites, satellites_players
 
@@ -91,34 +93,41 @@ def _parse_satellite(
 def sim_context_from_yaml(file_path: str):
     config = _load_config(file_path=file_path)
 
+    # rocket
+    assert len(config["agents"].keys()) == 0, "Only one player today"
     name = config["agents"].keys()[0]
     playername = PlayerName(name)
     x0 = RocketState(**config["agents"][name]["state"])
 
-    #target
-    conf_goal = config["agents"][name]["goal"]
-    x0_target = conf_goal["state"]
-    goal = RocketTarget(
-        target=x0_target, pos_tol=conf_goal["pos_tol"], vel_tol=conf_goal["vel_tol"]
-    )
-    missions = {playername: goal}
-    
-    # obstacles
+    # obstacles (planets + satellites)
     planets, satellites, satellites_npagents = _parse_planets(config)
     env_limits = LineString(config["boundary"]["corners"])
-    static_obstacles = planets.append(env_limits)
+    planets.append(env_limits)
 
-    static_obstacles: list[StaticObstacle] = [
-        StaticObstacle(shape=s) for s in static_obstacles
-    ]
-    # players
+    static_obstacles: list[StaticObstacle] = [StaticObstacle(shape=s) for s in planets]
+
+    # load goal
+    conf_goal = config["agents"][name]["goal"]
+    if conf_goal == "static":
+        x0_target = conf_goal["state"]
+        goal = RocketTarget(
+            target=x0_target, pos_tol=conf_goal["pos_tol"], vel_tol=conf_goal["vel_tol"]
+        )
+    elif conf_goal == "satellite":
+        # TODO goal from satellite
+        pass
+    else:
+        raise ValueError("Unrecognized goal type")
+    missions = {playername: goal}
+
+    # models & players
     players = {playername: RocketAgent()}
     models = {playername: RocketModel.default(x0)}
-    for i, (s, sagent) in enumerate(zip(satellites, satellites_npagents)):
-        p = PlayerName("P" + str(i))
-        players[p] = sagent
+    for p, s in satellites.items():
         models[p] = s
 
+    for p, sagent in satellites_npagents.items():
+        players[p] = sagent
 
     return SimContext(
         dg_scenario=DgScenario(static_obstacles=static_obstacles),
@@ -135,6 +144,8 @@ def sim_context_from_yaml(file_path: str):
 
 
 if __name__ == "__main__":
-    config_file = "src/pdm4ar/exercises_def/ex09/config_planet.yaml"
-    config = _load_config(config_file)
-    pprint(config)
+    configs = ["config_planet.yaml", "config_satellites.yaml", "config_mov_target.yaml"]
+    for c in configs:
+        config_file = "src/pdm4ar/exercises_def/ex09/" + c
+        config = _load_config(config_file)
+        pprint(config)
