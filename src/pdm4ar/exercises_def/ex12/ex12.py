9f75ed3921379db75d5369b6adff939cf77c43d0
yuesli <yuesli@student.ethz.ch>
2024-10-28 22:13:12 +0000

    add more scenarios

--- src/pdm4ar/exercises_def/ex12/ex12.py
+++ src/pdm4ar/exercises_def/ex12/ex12.py
@@ -82,10 +82,13 @@ def load_config_ex12(file_path: Path) -> Mapping:
 
 def get_exercise12():
     config_dir = Path(__file__).parent
-    name_1 = "config_1.yaml"
-    config_dict_1 = load_config_ex12(config_dir / name_1)
-    # config_dict_2 = load_config_ex12(config_dir / name_2)
-    test_values: List[SimContext] = get_sim_contexts(config_dict_1)
+    config_dict_1 = load_config_ex12(config_dir / "config_1.yaml")
+    config_dict_2 = load_config_ex12(config_dir / "config_2.yaml")
+    config_dict_3 = load_config_ex12(config_dir / "config_3.yaml")
+    test_values: List[SimContext] = []
+    test_values += get_sim_contexts(config_dict_1)
+    test_values += get_sim_contexts(config_dict_2)
+    test_values += get_sim_contexts(config_dict_3)
 
     return Exercise[SimContext, None](
         desc="Final '24 planning course exercise.",
