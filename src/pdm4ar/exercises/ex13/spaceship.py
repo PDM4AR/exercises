29d300dacde8676a6cdc8a5fc0219ddd6279c9cb
Mateo Haro <mateoharo05@gmail.com>
2025-08-24 04:02:35 +0000

    sympy hint

--- src/pdm4ar/exercises/ex13/spaceship.py
+++ src/pdm4ar/exercises/ex13/spaceship.py
@@ -26,7 +26,7 @@ class SpaceshipDyn:
 
         self.x = spy.Matrix(spy.symbols("x y psi vx vy dpsi delta m", real=True))  # states
         self.u = spy.Matrix(spy.symbols("thrust ddelta", real=True))  # inputs
-        self.p = spy.Matrix([spy.symbols('t_f', positive=True)])  # final time
+        self.p = spy.Matrix([spy.symbols("t_f", positive=True)])  # final time
 
         self.n_x = self.x.shape[0]  # number of states
         self.n_u = self.u.shape[0]  # number of inputs
@@ -36,11 +36,22 @@ class SpaceshipDyn:
         """
         Define dynamics and extract jacobians.
         Get dynamics for SCvx.
+        extract the state from self.x the following way:
         0x 1y 2psi 3vx 4vy 5dpsi 6delta 7m
         """
-        # Dynamics
-        f = spy.zeros(self.n_x, 1)
+        # TODO Modify dynamics
+        f = spy.zeros(self.n_x, 1)  # replace this line by computing the dynamics like in following example
+        "f[0] = ..."
+        "f[1] = ..."
+        "..."
+        "f[7] = ..."
 
+        # HINT for the dynamics: SymPy is a library for symbolic mathematics.
+        # Here you’ll need symbolic math, not numerical math.
+        # That means you should use SymPy functions (like sympy.sin, sympy.exp, …)
+        # together with symbols, instead of numerical functions from numpy or math.
+
+        # Jacobians and matrices of the system (don't need to change)
         A = f.jacobian(self.x)
         B = f.jacobian(self.u)
         F = f.jacobian(self.p)
