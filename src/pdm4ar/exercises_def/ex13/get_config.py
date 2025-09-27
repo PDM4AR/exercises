8aca47438b3b6c3a78b6c734cdf601d4828097fd
Mateo Haro <mateoharo05@gmail.com>
2025-09-27 12:47:59 +0000

    agent and planner sol modificiations to make it run

--- src/pdm4ar/exercises_def/ex13/get_config.py
+++ src/pdm4ar/exercises_def/ex13/get_config.py
@@ -5,11 +5,11 @@ def get_config() -> List[str]:
     "Choose manually which .yaml file to load and test."
 
     configs = [
-        #    "config_planet.yaml",
+        "config_planet.yaml",
         #    "config_satellites.yaml",
         #    "config_satellites_diff.yaml",
         # "config_local.yaml"
-        "test_asteroid.yaml"
+        # "test_asteroid.yaml"
     ]
 
     return configs
